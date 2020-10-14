eb42599c2479a999888218b07ba428933739d40b
suelyb <72868542+suelyb@users.noreply.github.com>
2020-10-14 17:36:37 -0300

    Update 1-init.sql

--- bi/schema/1-init.sql
+++ bi/schema/1-init.sql
@@ -2,3 +2,30 @@
 CREATE DATABASE dbmetabase;
 CREATE USER metabase WITH PASSWORD 'metabase@admin';
 GRANT ALL PRIVILEGES ON DATABASE dbmetabase TO metabase;
+
+
+-- BASE DE DADOS INOVA CNJ
+
+CREATE SCHEMA inovacnj
+    AUTHORIZATION inovacnj;
+
+-- CLASSE    
+CREATE TABLE inovacnj.classe
+(   cod numeric NOT NULL,
+    descricao character varying(200),
+    sigla character varying(20),
+    codpai numeric,
+    natureza character varying(10),
+    CONSTRAINT pk_classe PRIMARY KEY (cod)
+);
+
+-- ASSUNTO
+
+
+-- ORGAO_JULGADOR
+
+-- PROCESSO
+
+-- PROCESSOASSUNTO
+
+-- MOVIMENTO
