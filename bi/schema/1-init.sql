09b76445f9a54cf2995db7c027c243865d730d32
suelyb <72868542+suelyb@users.noreply.github.com>
2020-10-15 19:56:19 -0300

    Update 1-init.sql

--- bi/schema/1-init.sql
+++ bi/schema/1-init.sql
@@ -19,6 +19,9 @@ CREATE TABLE inovacnj.classe
     CONSTRAINT pk_classe PRIMARY KEY (cod)
 );
 
+ALTER TABLE inovacnj.classe
+  OWNER to inovacnj;
+
 -- ASSUNTO
 CREATE TABLE inovacnj.assunto
 (   cod numeric NOT NULL,
@@ -48,7 +51,7 @@ CREATE TABLE inovacnj.orgao_julgador
 ALTER TABLE inovacnj.orgao_julgador
     OWNER to inovacnj;
 
--- MOVIMENTO
+-- FASE
 CREATE TABLE inovacnj.movimento
 (   cod numeric NOT NULL,
     descricao character varying(200) NOT NULL,
@@ -60,19 +63,33 @@ ALTER TABLE inovacnj.movimento
     OWNER to inovacnj;
     
 -- PROCESSO
+CREATE TABLE inovacnj.processo
+(   npu character varying(30) ,
+    dtajuizamento timestamp,
+    codtribunal character varying(4) , 
+    codoj character varying(4),
+    grau character varying (4),
+    codassunto numeric,
+    assunto_principal boolean,
+    codclasse numeric, 
+    tramitacao character varying(1),   -- 1: Sistema Eletrônico - 2: Sistema Físico
+    CONSTRAINT pk_processo PRIMARY KEY (npu)
+);
 
+ALTER TABLE inovacnj.processo
+    OWNER to inovacnj;
+    
 -- ESFERA_JUSTICA
 CREATE TABLE inovacnj.esfera_justica
-(   cod character varying(1) NOT NULL,
-    descricao character varying(50) NOT NULL,
-    CONSTRAINT pk_ejust PRIMARY KEY (cod)
+(   cod character varying(1) NOT NULL,
+    descricao character varying(50) NOT NULL,
+    CONSTRAINT pk_ejust PRIMARY KEY (cod)
 );
 
 ALTER TABLE inovacnj.esfera_justica
-    OWNER to inovacnj; 
+    OWNER to inovacnj;
 
---
-
+-- TRIBUNAL
 CREATE TABLE inovacnj.tribunal
 (   cod character varying(4) NOT NULL,
     descricao character varying(50) NOT NULL,
