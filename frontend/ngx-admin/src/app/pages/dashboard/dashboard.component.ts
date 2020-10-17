fb392b5659553c5e47ac658064ad33cfee79747a
fabio.cruz.tavares <fabio.cruz.tavares@tjpe.jus.br>
2020-10-17 20:09:42 -0300

    projeto ngx-admin

---
+++ frontend/ngx-admin/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,213 @@
+import {Component, OnDestroy, ViewChild} from '@angular/core';
+import { NbGlobalLogicalPosition, NbGlobalPhysicalPosition, NbGlobalPosition, NbThemeService } from '@nebular/theme';
+import { SmartTableData } from 'app/@core/data/smart-table';
+import { LocalDataSource } from 'ng2-smart-table';
+import { takeWhile } from 'rxjs/operators' ;
+import { SolarData } from '../../@core/data/solar';
+
+interface CardSettings {
+  title: string;
+  iconClass: string;
+  type: string;
+}
+
+@Component({
+  selector: 'ngx-dashboard',
+  styleUrls: ['./dashboard.component.scss'],
+  templateUrl: './dashboard.component.html',
+})
+
+export class DashboardComponent implements OnDestroy {
+  
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+      },
+      firstName: {
+        title: 'First Name',
+        type: 'string',
+      },
+      lastName: {
+        title: 'Last Name',
+        type: 'string',
+      },
+      username: {
+        title: 'Username',
+        type: 'string',
+      },
+      email: {
+        title: 'E-mail',
+        type: 'string',
+      },
+      age: {
+        title: 'Age',
+        type: 'number',
+      },
+    },
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  private alive = true;
+  position: NbGlobalPosition = NbGlobalPhysicalPosition.TOP_RIGHT;
+  date = new Date();
+  date2 = new Date();
+
+  fruits : string[] = [
+    'Lemons',
+    'Raspberries',
+    'Strawberries',
+    'Blackberries',
+    'Kiwis',
+    'Grapefruit',
+    'Avocado',
+    'Watermelon',
+    'Cantaloupe',
+    'Oranges',
+    'Peaches',
+  ];  
+   
+  positions: string[] = [
+    NbGlobalPhysicalPosition.TOP_RIGHT,
+    NbGlobalPhysicalPosition.TOP_LEFT,
+    NbGlobalPhysicalPosition.BOTTOM_LEFT,
+    NbGlobalPhysicalPosition.BOTTOM_RIGHT,
+    NbGlobalLogicalPosition.TOP_END,
+    NbGlobalLogicalPosition.TOP_START,
+    NbGlobalLogicalPosition.BOTTOM_END,
+    NbGlobalLogicalPosition.BOTTOM_START,
+  ];
+
+  results = [
+    { name: 'Germany', value: 8940 },
+    { name: 'USA', value: 5000 },
+    { name: 'France', value: 7200 },
+  ];
+  showLegend = true;
+  showXAxis = true;
+  showYAxis = true;
+  xAxisLabel = 'Country';
+  yAxisLabel = 'Population';
+  colorScheme: any;
+  themeSubscription: any;
+
+  solarValue: number;
+  lightCard: CardSettings = {
+    title: 'Light',
+    iconClass: 'nb-lightbulb',
+    type: 'primary',
+  };
+  rollerShadesCard: CardSettings = {
+    title: 'Roller Shades',
+    iconClass: 'nb-roller-shades',
+    type: 'success',
+  };
+  wirelessAudioCard: CardSettings = {
+    title: 'Wireless Audio',
+    iconClass: 'nb-audio',
+    type: 'info',
+  };
+  coffeeMakerCard: CardSettings = {
+    title: 'Coffee Maker',
+    iconClass: 'nb-coffee-maker',
+    type: 'warning',
+  };
+
+  statusCards: string;
+
+  commonStatusCardsSet: CardSettings[] = [
+    this.lightCard,
+    this.rollerShadesCard,
+    this.wirelessAudioCard,
+    this.coffeeMakerCard,
+  ];
+
+  statusCardsByThemes: {
+    default: CardSettings[];
+    cosmic: CardSettings[];
+    corporate: CardSettings[];
+    dark: CardSettings[];
+  } = {
+    default: this.commonStatusCardsSet,
+    cosmic: this.commonStatusCardsSet,
+    corporate: [
+      {
+        ...this.lightCard,
+        type: 'warning',
+      },
+      {
+        ...this.rollerShadesCard,
+        type: 'primary',
+      },
+      {
+        ...this.wirelessAudioCard,
+        type: 'danger',
+      },
+      {
+        ...this.coffeeMakerCard,
+        type: 'info',
+      },
+    ],
+    dark: this.commonStatusCardsSet,
+  };
+
+  constructor(private themeService: NbThemeService,
+              private solarService: SolarData,
+              private service: SmartTableData) {
+    this.themeService.getJsTheme()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(theme => {
+        this.statusCards = this.statusCardsByThemes[theme.name];
+    });
+
+    this.solarService.getSolarData()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe((data) => {
+        this.solarValue = data;
+      });
+
+    this.themeSubscription = this.themeService.getJsTheme().subscribe(config => {
+      const colors: any = config.variables;
+      this.colorScheme = {
+        domain: [colors.primaryLight, colors.infoLight, colors.successLight, colors.warningLight, colors.dangerLight],
+      };
+    });
+
+    const data = this.service.getData();
+    this.source.load(data);
+
+  }
+
+  ngOnDestroy(): void {
+    this.alive = false;
+    this.themeSubscription.unsubscribe();
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
+
+  
+
+
+}
